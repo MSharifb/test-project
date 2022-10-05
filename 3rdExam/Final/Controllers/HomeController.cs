3e9033d635f0bdd134b21e4d8ddefb4f4f21157c
MSharifb <96659557+MSharifb@users.noreply.github.com>
2022-10-06 00:05:58 +0600

    commit all file

---
+++ 3rdExam/Final/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Final.Models;
+
+namespace Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        FinalEntities db = new FinalEntities();
+        public ActionResult Index()
+        {
+            var req = db.Requests.OrderByDescending(r => r.ReqID).Take(10);
+            ViewBag.request = req;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 3rdExam/ThirdExam/ThirdExam/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThirdExam.Models;
+
+namespace ThirdExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        ThirdExamMorningEntities db = new ThirdExamMorningEntities();
+        public ActionResult Index()
+        {
+
+            var featurs = db.Posts.OrderByDescending(p => p.ID).ToList().Take(2);
+            var category = db.Categories.ToList();
+
+
+            ViewBag.categories = category;
+            ViewBag.feat_post = featurs;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
